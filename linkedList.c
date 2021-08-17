402bd925ccff7aae03942ed5128cc1269727f501
sudeshiuthpala <sudeshiuthpala98@gmail.com>
2021-08-18 00:57:42 +0530

    linked list implementation complete

--- linkedList.c
+++ linkedList.c
@@ -37,15 +37,7 @@ void createlist(int n){
 	}
 }
 
-struct node* display(struct node *start){
-	node* ptr;
-	ptr=start;
-	while(ptr!=NULL){
-		printf("%d\n",ptr->data);
-		ptr=ptr->next;
-	}
-	return start;
-}
+
 
 int countLink(node* start){
 	int count=0;
@@ -75,9 +67,111 @@ struct node* search(struct node *start,int val){
 	return pos;
 }
 
+node* insert_beg(node* start,int num){
+	node* newnode;
+	newnode=(node *)malloc(sizeof(node));
+	newnode->data=num;
+	newnode->next=start;
+	start=newnode;
+	return start;
+}
+
+node* deletion_beg(node* start){
+	node* ptr;
+	ptr=start;
+	start=start->next;
+	free(ptr);
+	return start;
+}
+
+node* insert_end(node* start,int num){
+	node *newnode,*ptr;
+	newnode=(node *)malloc(sizeof(node));
+	newnode->data=num;
+	newnode->next=NULL;
+	ptr=start;
+	while(ptr->next!=NULL){
+		ptr=ptr->next;
+	}
+	ptr->next=newnode;
+	return start;
+}
+node* deletion_end(node* start){
+	node *preptr,*ptr;
+	ptr=start;
+	while(ptr->next!=NULL){
+		preptr=ptr;
+		ptr=ptr->next;
+	}
+	ptr->next=NULL;
+	free(ptr);
+	return start;
+}
+
+node* insert_after(node*start,int val,int num){
+	node *newnode,*ptr,*preptr;
+	newnode=(node *)malloc(sizeof(node));
+	newnode->data=num;
+	ptr=start;
+	preptr=ptr;
+	while(preptr->data!=val){
+		preptr=ptr;
+		ptr=ptr->next;
+	}
+	preptr->next=newnode;
+	newnode->next=ptr;
+	return start;
+}
+node* insert_before(node*start,int val,int num){
+	node *newnode,*ptr,*preptr;
+	newnode=(node *)malloc(sizeof(node));
+	newnode->data=num;
+	ptr=start;
+	//preptr=ptr;
+	while(ptr->data!=val){
+		preptr=ptr;
+		ptr=ptr->next;
+	}
+	preptr->next=newnode;
+	newnode->next=ptr;
+	return start;
+}
+node *delete_node(node *start,int val)
+{
+	node *ptr, *preptr;
+	ptr = start;
+	if(ptr -> data == val)
+	{
+		start = delete_beg(start);
+		return start;
+	}
+	else
+	{
+		while(ptr -> data != val)
+		{
+			preptr = ptr;
+			ptr = ptr -> next;
+		}
+		preptr -> next = ptr -> next;
+		free(ptr);
+		return start;
+	}
+}
+node* display(node *start){
+	node* ptr;
+	ptr=start;
+	while(ptr!=NULL){
+		printf("%d\n",ptr->data);
+		ptr=ptr->next;
+	}
+	return start;
+}
+
 int main(){
-	createlist(5);
+	createlist(6);
+	//display(start);
+	//printf("%d\n",countLink(start));
+	//printf("%d\n",search(start,2));
+	deletion_beg(start);
 	display(start);
-	printf("%d\n",countLink(start));
-	printf("%d\n",search(start,2));
 }
